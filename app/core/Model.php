569a612d66a82b4fae13f32b1c0d3e29c4f294d2
Farshad135110 <farshadfahumy@gmail.com>
2025-02-12 14:43:34 +0530

    comment

--- app/core/Model.php
+++ app/core/Model.php
@@ -210,9 +210,8 @@ trait Model
         $query = "INSERT INTO $this->table (" . implode(",", $keys) . ") VALUES (:" . implode(",:", $keys) . ")";
         return $this->query($query, $data);
     }
-<<<<<<< HEAD
 
-=======
+
 //dont change
     /**
      * Update an existing record by ID
@@ -222,7 +221,7 @@ trait Model
      * @param string $id_column The column to use for the WHERE clause
      * @return bool
      */
->>>>>>> Farshad
+
     public function update($id, $data, $id_column = 'id')
     {
         if (!empty($this->allowedColumns)) {
